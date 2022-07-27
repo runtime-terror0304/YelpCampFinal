5ef4d3fcf55ecc3bfffe5ac6eee557d063320537
Rachit Puri <rachit200177@gmail.com>
2022-07-27 16:47:59 +0530

    cluster map center issue

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 container: 'cluster-map',
 style: 'mapbox://styles/mapbox/dark-v10',
-center: [-103.5917, 40.6699],
+center: [78.9629, 20.5937],
 zoom: 3
 });
 
